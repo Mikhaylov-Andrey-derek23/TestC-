c559d9b9c9e359d7c66db92a2fd2b689202c0037
Derek23 <derek23@yandex.ru>
2020-03-11 21:37:44 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace lessson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your name?");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine($"Your name {name}");
+            Console.WriteLine("Click Enter for exit");
+            Console.Read();
+            Thread.Sleep(10000);
+            Console.WriteLine("By By By");
+        }
+    }
+}
